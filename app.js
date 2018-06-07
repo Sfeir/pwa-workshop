24e22a34b589b5f70e3d981b4c55470cbeb27ae3
Noël Macé <contact@noelmace.com>
2018-06-07 11:29:50 +0200

    feat(web-app): install button

--- app.js
+++ app.js
@@ -1,6 +1,40 @@
 let PLAYERS = [];
 const WRAPPER = document.querySelector('.players');
 
+let deferredPrompt;
+const btnAdd = document.querySelector('.btn-add');
+
+window.addEventListener('beforeinstallprompt', function(e) { 
+  // log the platforms provided as options in an install prompt 
+  // console.log(e.platforms); // e.g., ["web", "android", "windows"] 
+  // e.userChoice.then(function(outcome) { 
+  //   console.log(outcome); // either "accepted" or "dismissed"
+  // }, function(e) {
+  //   console.log(e);
+  // }); 
+  e.preventDefault();
+  deferredPrompt = e;
+  btnAdd.style.display = 'block';
+});
+
+
+btnAdd.addEventListener('click', (e) => {
+  // hide our user interface that shows our A2HS button
+  btnAdd.style.display = 'none';
+  // Show the prompt
+  deferredPrompt.prompt();
+  // Wait for the user to respond to the prompt
+  deferredPrompt.userChoice
+    .then((choiceResult) => {
+      if (choiceResult.outcome === 'accepted') {
+        console.log('User accepted the A2HS prompt');
+      } else {
+        console.log('User dismissed the A2HS prompt');
+      }
+      deferredPrompt = null;
+    });
+});
+
 fetch('./players.json')
   .then(response => {
     return response.json()
@@ -170,3 +204,4 @@ if ('serviceWorker' in navigator && 'PushManager' in window) {
   console.warn('Push messaging is not supported');
   pushButton.textContent = 'Push Not Supported';
 }
+
